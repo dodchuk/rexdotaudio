93b586fce41734c0a463732ac8df0005a3bd975f
dodchukdotfinsight <max.dodchuk@finsight.com>
2025-06-09 01:02:42 +0000

    fix: <head>

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,6 +12,11 @@ interface RootLayoutProps {
 export default function RootLayout({ children }: RootLayoutProps) {
   return (
     <html>
+    <head>
+      <title>Інструмент Рекс</title>
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <link rel="icon" href="/favicon.ico" />
+    </head>
     <body>
       <Bootstrap children={ children } />
       <SpeedInsights />
